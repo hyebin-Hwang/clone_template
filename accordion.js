f6ec8e930ab125a27a30bd3c48cace939914ed0a
hyebin-Hwang <67263146+hyebin-Hwang@users.noreply.github.com>
2020-07-14 18:56:24 +0900

    Version 3
    add to accordion menu and map API

---
+++ accordion.js
@@ -0,0 +1,19 @@
+const accHeader = document.querySelectorAll(`.accordion_headerJs`);
+
+accHeader.forEach(accHeader =>{
+    accHeader.addEventListener("click",event =>{
+        const activeHeader = document.querySelector(`.accordion_headerJs.active`);
+        if(activeHeader && activeHeader !== accHeader){
+            activeHeader.classList.toggle("active");
+            activeHeader.nextElementSibling.style.maxHeight = 0;
+        }
+
+        accHeader.classList.toggle("active");
+        const itemBody = accHeader.nextElementSibling;
+        if(accHeader.classList.contains("active")){
+            itemBody.style.maxHeight = itemBody.scrollHeight + "px";
+        }else {
+            itemBody.style.maxHeight = 0;
+        }
+    })
+})
---
+++ map.js
@@ -0,0 +1,7 @@
+var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+var options = { //지도를 생성할 때 필요한 기본 옵션
+	center: new kakao.maps.LatLng(37.5327518,126.7327281), //지도의 중심좌표.
+	level: 3 //지도의 레벨(확대, 축소 정도)
+};
+
+var map = new kakao.maps.Map(container, options);
